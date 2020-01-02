efd9fe27aeae81cb910b40d3a1b00a2d4736d9a9
Beza Sirak <sirak.bezawit@gmail.com>
2020-01-02 12:57:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,7 +21,7 @@ def using_concat (array, string)
   array.concat(string)
 end
 def using_insert(array, element)
-  array.insert(4,element)
+  array.insert(4, element)
 end
 def using_uniq(array)
   array.uniq
